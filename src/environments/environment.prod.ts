980631f6a7b75372c19e1ec6bd7f5534aca5d878
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2019-04-20 22:27:51 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDHxprioCtQLyNVpcmev7DzlZoxwzcrmnE',
-    authDomain: 'integrar-firebase-y-angular.firebaseapp.com',
-    databaseURL: 'https://integrar-firebase-y-angular.firebaseio.com',
-    projectId: 'integrar-firebase-y-angular',
-    storageBucket: 'integrar-firebase-y-angular.appspot.com',
-    messagingSenderId: '615672514792'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
